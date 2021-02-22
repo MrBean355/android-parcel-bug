95ef4d6f3779170cb89ad418286a81e2d67ed69b
Mike Johnston <mrbean355@gmail.com>
2021-02-22 20:38:29 +0200

    Initial commit

---
+++ app/src/main/java/com/github/mrbean355/android/parcelbug/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.mrbean355.android.parcelbug
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
